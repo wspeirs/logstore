078fa1e39fbccef00b4f929cc09a4112366a71d4
William Speirs <bill.speirs@gmail.com>
2018-01-13 16:53:21 -0500

    Added in tombstone and message length

--- src/message_file.rs
+++ src/message_file.rs
@@ -16,14 +16,15 @@ use std::io::{Read, Write, ErrorKind, Seek, SeekFrom};
 use std::io::Error as IOError;
 
 /// This struct represents the on-disk format of the MessageFile
-/// The messages follow the VV, where VV is the version number:
+/// The messages follow the VV, where VV is the version number and
+/// 0xTT is the if the message has been tombstoned:
 /// |-------------------------------------------|
 /// | 0x4C 0x4F 0x47 0x53 | 0x54 0x4F 0x52 0xVV |
 /// | L    O    G    S    | T    O    R    0xVV |
 /// |-------------------------------------------|
 /// | records in the file, -1 = not saved       |
 /// |-------------------------------------------|
-/// | Tombstone: 0 or 1   | Message size        |
+/// | 0xTT | Msg size as u32    | message       |
 /// |-------------------------------------------|
 /// | message in JSON                           |
 /// |-------------------------------------------|
@@ -74,7 +75,7 @@ impl MessageFile {
         trace!("Attempting to open message file: {}", msg_file_path);
 
         // open the file, and return if an error is encountered
-        let msg_file = OpenOptions::new().read(true).write(true).create(true).open(&msg_file_path)?;
+        let msg_file = OpenOptions::new().read(true).append(true).create(true).open(&msg_file_path)?;
 
         debug!("Created MessageFile with {}", msg_file_path);
 
@@ -95,39 +96,41 @@ impl MessageFile {
 
         trace!("Checking all fields for __");
 
-        // check to see if there are any restricted fields
-        if json.keys().any(|k| k.starts_with("__")) {
-            return Err(From::from("Illegal fields in message; fields cannot start with __: ".to_owned() + message));
-        }
-
-        // get a sorted vector of keys to form a canonical representation
-        let mut sorted_keys = Vec::<&String>::new();
-        { json.keys().collect::<Vec<&String>>(); } // wrapped in { } because we borrow json as immutable
-        sorted_keys.sort_unstable();
-
         let mut canoncial_json = String::from("{");
 
-        // go through each sorted key, and convert it into a string based upon type
-        // or return an error if a nested JSON object is found
-        for key in sorted_keys.into_iter() {
-            let value = match json.get(key).unwrap().to_owned() {
-                Value::Object(_) => Err(make_json_error("Nested JSON Objects are not allowed")),
-                Value::Null => Ok(String::from("")),
-                Value::Bool(b) => Ok(b.to_string()),
-                Value::Number(n) => Ok(n.to_string()),
-                Value::String(s) => Ok(s),
-                Value::Array(v) => Ok(v.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(","))
-            }?;
+        { // this wraps all the immutable borrows
+            // check to see if there are any restricted fields
+            if json.keys().any(|k| k.starts_with("__")) {
+                return Err(From::from("Illegal fields in message; fields cannot start with __: ".to_owned() + message));
+            }
 
-            // add key: value
-            canoncial_json.push_str(&format!("{}:{},", key.to_owned(), value));
+            // get a sorted vector of keys to form a canonical representation
+            let mut sorted_keys = json.keys().collect::<Vec<&String>>();
+
+            sorted_keys.sort_unstable();
+
+            // go through each sorted key, and convert it into a string based upon type
+            // or return an error if a nested JSON object is found
+            for key in sorted_keys.into_iter() {
+                let value = match json.get(key).unwrap().to_owned() {
+                    Value::Object(_) => Err(make_json_error("Nested JSON Objects are not allowed")),
+                    Value::Null => Ok(String::from("")),
+                    Value::Bool(b) => Ok(b.to_string()),
+                    Value::Number(n) => Ok(n.to_string()),
+                    Value::String(s) => Ok(s),
+                    Value::Array(v) => Ok(v.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(","))
+                }?;
+
+                // add key: value
+                canoncial_json.push_str(&format!("{}:{},", key.to_owned(), value));
+            }
         }
 
         canoncial_json.pop(); // remove trailing comma
         canoncial_json.push_str("}");
 
         debug!("Canonical: {}", canoncial_json);
-        println!("{}", canoncial_json);
+        println!("Canonical: {}", canoncial_json);
 
         // construct our hash function
         let mut hash: XxHash = XxHash::with_seed(0xBEDBEEF);
@@ -146,11 +149,18 @@ impl MessageFile {
 
         let final_message = serde_json::to_string(&json)?;
 
-        // write the data to the file, matching on the error
-        match self.fd.write(&final_message.as_bytes()) {
-            Ok(_) => Ok( () ),
-            Err(e) => Err(From::from(e))
-        }
+        println!("Final Message: {} {}", final_message.len(), final_message);
+
+        // write out that we're not tombstoning this
+        self.fd.write(&[0x00 as u8])?;
+
+        // write out the size of the message
+        self.fd.write(&to_array(final_message.len() as u64))?;
+
+        // write the data to the file
+        self.fd.write(&final_message.as_bytes())?;
+
+        return Ok( () )
     }
 }
 
