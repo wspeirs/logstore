919f720632349f3c5424b08ee55379d0754ac3a6
William Speirs <bill.speirs@gmail.com>
2018-01-13 17:33:14 -0500

    Added file header

--- src/message_file.rs
+++ src/message_file.rs
@@ -2,41 +2,28 @@ extern crate serde_json;
 extern crate twox_hash;
 extern crate base32;
 extern crate time;
+extern crate byteorder;
 
 use serde_json::{Value, Map};
 use serde_json::Error as JsonError;
 use serde_json::error::ErrorCode;
 use self::twox_hash::XxHash;
 use self::base32::Alphabet;
+use self::byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 
+use std::io::Cursor;
 use std::hash::Hasher;
 use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write, ErrorKind, Seek, SeekFrom};
 use std::io::Error as IOError;
 
-/// This struct represents the on-disk format of the MessageFile
-/// The messages follow the VV, where VV is the version number and
-/// 0xTT is the if the message has been tombstoned:
-/// |-------------------------------------------|
-/// | 0x4C 0x4F 0x47 0x53 | 0x54 0x4F 0x52 0xVV |
-/// | L    O    G    S    | T    O    R    0xVV |
-/// |-------------------------------------------|
-/// | records in the file, -1 = not saved       |
-/// |-------------------------------------------|
-/// | 0xTT | Msg size as u32    | message       |
-/// |-------------------------------------------|
-/// | message in JSON                           |
-/// |-------------------------------------------|
-/// | ...                                       |
-/// |-------------------------------------------|
-
 // this is kinda clunky :-\
 fn make_json_error(msg: &str) -> JsonError {
-    return JsonError::syntax(ErrorCode::Message(String::from(msg).into_boxed_str()), 0, 0)
+    return JsonError::syntax(ErrorCode::Message(String::from(msg).into_boxed_str()), 0, 0);
 }
 
-fn to_array(x: u64) -> [u8; 8] {
+fn u64_bytes(x: u64) -> [u8; 8] {
     return [
         ((x >> 56) & 0xff) as u8,
         ((x >> 48) & 0xff) as u8,
@@ -46,7 +33,16 @@ fn to_array(x: u64) -> [u8; 8] {
         ((x >> 16) & 0xff) as u8,
         ((x >> 8) & 0xff) as u8,
         (x & 0xff) as u8
-    ]
+    ];
+}
+
+fn u32_bytes(x: u32) -> [u8; 4] {
+    return [
+        ((x >> 24) & 0xff) as u8,
+        ((x >> 16) & 0xff) as u8,
+        ((x >> 8) & 0xff) as u8,
+        (x & 0xff) as u8
+    ];
 }
 
 fn get_ts() -> (i64) {
@@ -60,7 +56,27 @@ fn get_ts() -> (i64) {
 ///
 pub struct MessageFile {
     fd: File, // the actual message file
+    num_messages: u32, // the number of messages in the file
+    //TODO: Need to add a destructor as this value is never saved
 }
+
+/// This struct represents the on-disk format of the MessageFile
+/// The messages follow the VV, where VV is the version number and
+/// 0xTT is the if the message has been tombstoned:
+/// |-------------------------------------------|
+/// | 0x4C 0x4F 0x47 0x53 | 0x54 0x4F 0x52 0xVV |
+/// | L    O    G    S    | T    O    R    0xVV |
+/// |-------------------------------------------|
+/// | num msgs in file    | 0xTT | msg size ... |
+/// |-------------------------------------------|
+/// | ... | message  in JSON ...                |
+/// |-------------------------------------------|
+/// | ... message in JSON                       |
+/// |-------------------------------------------|
+/// | ...                                       |
+/// |-------------------------------------------|
+
+const VERSION: u8 = 0x1;
 
 impl MessageFile {
     pub fn new(file_path: &str) -> Result<MessageFile, Box<Error>> {
@@ -75,21 +91,48 @@ impl MessageFile {
         trace!("Attempting to open message file: {}", msg_file_path);
 
         // open the file, and return if an error is encountered
-        let msg_file = OpenOptions::new().read(true).append(true).create(true).open(&msg_file_path)?;
+        let mut msg_file = OpenOptions::new().read(true).append(true).create(true).open(&msg_file_path)?;
+        let mut num_messages = 0;
+
+        // if we're opening a new file, write the header info
+        if msg_file.metadata()?.len() == 0 {
+            msg_file.write(b"LOGSTOR")?; // write the header/magic
+            msg_file.write_u8(VERSION)?;
+            msg_file.write_u32::<LittleEndian>(0x00)?;
+        } else {
+            let mut magic = vec![0; 7];
+
+            msg_file.read_exact(&mut magic)?;
+
+            if "LOGSTOR".as_bytes() != magic.as_slice() {
+                return Err(From::from("Invalid magic header for file"));
+            }
+
+            let version = msg_file.read_u8()?;
+
+            if VERSION != version {
+                return Err(From::from("Wrong version number"));
+            }
+
+            num_messages = msg_file.read_u32::<LittleEndian>()?;
+        }
 
         debug!("Created MessageFile with {}", msg_file_path);
 
         // create our structure and return it wrapped in an Ok()
-        return Ok(MessageFile { fd: msg_file });
+        return Ok(MessageFile {
+            fd: msg_file,
+            num_messages: num_messages,
+        });
     }
 
     pub fn add_message(&mut self, message: &str) -> Result<(), Box<Error>> {
         trace!("Attempting to parse JSON");
 
-        let v:Value = serde_json::from_str(message)?;
+        let v: Value = serde_json::from_str(message)?;
 
         // unpack the resulting value, and ensure it's an object
-        let mut json:Map<String, Value> = match v {
+        let mut json: Map<String, Value> = match v {
             Value::Object(x) => Ok(x),
             _ => Err(make_json_error("Messages must be JSON objects"))
         }.unwrap();
@@ -137,7 +180,7 @@ impl MessageFile {
 
         hash.write(canoncial_json.as_bytes());
 
-        let id = base32::encode(Alphabet::RFC4648{padding: false}, &to_array(hash.finish()));
+        let id = base32::encode(Alphabet::RFC4648 { padding: false }, &u64_bytes(hash.finish()));
 
         println!("ID: {}", id);
 
@@ -155,12 +198,18 @@ impl MessageFile {
         self.fd.write(&[0x00 as u8])?;
 
         // write out the size of the message
-        self.fd.write(&to_array(final_message.len() as u64))?;
+        self.fd.write(&u32_bytes(final_message.len() as u32))?;
 
         // write the data to the file
         self.fd.write(&final_message.as_bytes())?;
 
-        return Ok( () )
+        // flush to disk
+        self.fd.flush();
+
+        // update the number of messages written
+        self.num_messages += 1;
+
+        return Ok(());
     }
 }
 
