1c2f19970034f172a779b3f4c1accbb4bb4b14e6
William Speirs <bill.speirs@gmail.com>
2018-02-28 17:59:23 -0500

    Switched up how things load, now can handle Ctrl-C

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,8 @@ mod http_server;
 use std::collections::HashMap;
 use std::thread;
 use std::time;
+use std::sync::{Arc, Mutex};
+use std::path::Path;
 use log::Level;
 
 use chan_signal::Signal;
@@ -50,6 +52,7 @@ use futures::executor;
 use rpc_server::run_rpc_server;
 use http_server::configure_http_server;
 use rpc_server::RPCClient;
+use data_manager::DataManager;
 
 fn main() {
     simple_logger::init_with_level(Level::Debug).unwrap(); // this will panic on error
@@ -57,13 +60,15 @@ fn main() {
     // signal channel to handle Ctrl-C
     let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
 
-    // create the core for the clients and HTTP Server
-    let mut core = Core::new().unwrap();
+    // create our DataManager
+    let dm = Arc::new(Mutex::new(DataManager::new(Path::new("/tmp")).unwrap()));
+
+    let dm_c = dm.clone();
 
     // spaw off our RPC server
     let handler = thread::Builder::new()
         .name("rpc server".to_string())
-        .spawn(move || run_rpc_server())
+        .spawn(move || run_rpc_server(dm_c))
         .unwrap();
 
     // hackie
@@ -71,35 +76,36 @@ fn main() {
 
     debug!("Creating client map");
 
-    // construct the server info
-    let mut server_info: HashMap<u32, RPCClient> = HashMap::new();
 
-    // insert bogus servers
-    server_info.insert(
-        0,
-        RPCClient::new("127.0.0.1:12345".to_string(), &mut core),
-    );
-//    server_info.insert(
-//        1,
-//        RPCClient::new("127.0.0.1:2345".to_string(), &mut core),
-//    );
+    thread::spawn(|| {
+        // create the core for the clients and HTTP Server
+        let mut core = Core::new().unwrap();
 
+        // construct the server info
+        let mut server_info: HashMap<u32, RPCClient> = HashMap::new();
 
-    let http_handle = core.handle();
+        // insert bogus servers
+        server_info.insert(
+            0,
+            RPCClient::new("127.0.0.1:12345".to_string(), &mut core),
+        );
+    //    server_info.insert(
+    //        1,
+    //        RPCClient::new("127.0.0.1:2345".to_string(), &mut core),
+    //    );
 
-    //    debug!("GOT HERE");
+        let http_handle = core.handle();
 
-    configure_http_server(&http_handle, server_info);
+        configure_http_server(&http_handle, server_info);
 
-//    core.run(future::empty::<(), ()>()).unwrap();
-    core.run(future::ok::<_,()>({
-        let ret = signal.recv().unwrap();
-        debug!("GOT SIGNAL");
+        core.run(future::empty::<(), ()>()).unwrap();
+    });
 
-        ret
-    })).unwrap();
+    let ret = signal.recv().unwrap();
 
-//    debug!("Waiting on join");
 //    handler.join().unwrap();
-    debug!("After join");
+//    debug!("After join");
+
+    dm.lock().unwrap().close();
+    info!("Data Manager closed");
 }
